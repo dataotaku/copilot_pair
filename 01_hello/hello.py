4eee4012c7dc7a9bbb402cae02aaf08052c7ed16
unho.chang <capcock@snu.ac.kr>
2024-07-08 15:59:40 +0900

    refactor: Refactor hello.py to use a main function
    The code changes refactor hello.py to use a main function. This improves code organization and readability by separating the argument parsing and the main logic into separate functions. The main function is now called when the script is executed directly, allowing for better reusability of the code.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -5,10 +5,16 @@ Date   : 2024-07-08
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Say hello")
-parser.add_argument(
-    "-n", "--name", default="World", metavar="name", help="Name to greet"
-)
-args = parser.parse_args()
 
-print("Hello, " + args.name + "!")
+def main():
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", default="World", metavar="name", help="Name to greet"
+    )
+    args = parser.parse_args()
+
+    print("Hello, " + args.name + "!")
+
+
+if __name__ == "__main__":
+    main()
