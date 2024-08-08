f90b1373f3a267ca6cffd26648c764bdeb0953cd
unho.chang <capcock@snu.ac.kr>
2024-08-08 17:10:07 +0900

    rhymer 문제풀이 준비완료 커밋

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -6,6 +6,7 @@ Purpose: Python Coding Club
 """
 
 import argparse
+import re
 
 
 # --------------------------------------------------
@@ -13,38 +14,11 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Python Coding Club',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description='Make rhyming "words"',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("word", metavar="word", help="A word to rhyme")
 
     return parser.parse_args()
 
@@ -54,19 +28,26 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    word = args.word
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+
+# ----------------------------------------------------
+def stammer():
+    """Return leading consonants (if any), and 'stem' of word"""
+    pass
+
+
+# ----------------------------------------------------
+def test_stemmer():
+    """test the stammer function"""
+    assert stammer("") == ("", "")
+    assert stammer("cake") == ("c", "ake")
+    assert stammer("chair") == ("ch", "air")
+    assert stammer("APPLE") == ("", "apple")
+    assert stammer("RDNZL") == ("rdnzl", "")
+    assert stammer("123") == ("123", "")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
